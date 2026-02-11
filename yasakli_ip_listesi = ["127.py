5035b786ab2c0692f29958cf5209c874a460b953
burak karataş <burak81mcc@gmail.com>
2026-02-11 16:40:46 +0300

    Update yasakli_ip_listesi = ["127.py

--- yasakli_ip_listesi = ["127.py
+++ yasakli_ip_listesi = ["127.py
@@ -1,26 +1,47 @@
-yasakli_ip_listesi = ["127.0.0.1", "192.168.1.1", "10.0.0.1", "8.8.8.8"]
-
-sifre = input("Sisteme erismek icin sifreyi giriniz: ")
-
-if sifre == "cyber123":
-    print("[+] Giriş başarili! Analiz araçlari yükleniyor...")
+root_password = ("123" , "456")
+admin_password = ("789" , "741")
+black_list = ("128.0.0.1" , "129.0.1.0" , "127.1.0.0")
+admin_list = ("255.128.0.1" , "123.248.1.0")
+girilen_IP = str(input("Lütfen IP adresinizi giriniz: "))
+if girilen_IP in black_list:
+    print("Girisiniz Reddedildi (yasakli IP)")
+elif girilen_IP in admin_list:
+    sifre = input("Admin girisiniz onaylandi LÜtfen şifrenizi giriniz: ")
+    if sifre in root_password:
+        root_paneli = input("""Root girişi algilandi Yetki: "HERŞEY SERBEST" Yapacağiniz işlemi seçiniz!!!\nKonum bulmak için lütfen 1'e basiniz\nIP sorgulamak için lütfen 2'e basiniz\nTC sorgulamak için lütfen 3'e basiniz
+""") 
+        if root_paneli == "1":
+            sorgu_IPsi = input("Lütfen konumunu bulmak istediğiniz IP adresiniz giriniz: ")
+            print("Girmiş olduğunuz IP adresi: TÜRKİYE Düzce/Merkez aziziye mahallesi 883. sokak")
+        elif root_paneli == "2":
+            sorgu_IPsi = input("Lütfen Sorgulamak istediğiniz IP adresnizi giriniz: ")
+            print("UYARI: Bu IP adresi 14 farkli siber saldiri veritabaninda 'Zararli' olarak işaretlenmiş! Erişim derhal kisitlaniyor")
+        elif root_paneli == "3":
+            sorgu_TCsi = input("Lütfen sorgulamak istediğiniz TC'yi giriniz: ")
+            print("TC Kimlik Numarasi doğrulaniyor... [OK]\nKayit Bulundu: Şahis hakkinda 'Kritik Seviye' güvenlik soruşturmasi devam etmektedir. Sistemsel erişim izni: Kisitli")
+        else:
+            print("Yanliş seçeneği seçtiniz. Oturumuz kapatiliyor!!!")
     
-    hedef_ip = input("Lutfen tarama yapilacak hedef IP adresini giriniz: ")
-    tarama_modu = input("Tarama modunu seçiniz (Hizli/Detayli): ")
-
-    print("\n--- İŞLEM BAŞLATILIYOR ---")
-    print("Hedef IP:", hedef_ip)
-    print("Seçilen Mod:", tarama_modu)
-
-    if hedef_ip in yasakli_ip_listesi:
-        print(f"\n[!] HATA: {hedef_ip} adresi yasakli listededir!")
-        print("[!] İşlem güvenlik nedeniyle durduruldu.")
-    else:
-        print("\n--- ANALİZ BAŞLATILDI ---")
-        print(f"Hedef: {hedef_ip}")
-        print("Durum: %100 Güvenli. Tarama izni verildi.")
-
+    elif sifre in admin_password:
+        admin_paneli = input("""Admin girişi algilandi Yetki: KISITLI "SERBETLİK" Yapacağiniz işlemi seçiniz!!!\nKonum bulmak için lütfen 1'e basiniz\nIP sorgulamak için lütfen 2'e basiniz
+""")
+        if admin_paneli == "1":
+            sorgu_IPsi = input("Lütfen konumunu bulmak istediğiniz IP adresiniz giriniz: ")
+            print("Girmiş olduğunuz IP adresi: TÜRKİYE Düzce/Merkez aziziye mahallesi 883. sokak no:13 daire:3 kat:1")
+        elif admin_paneli == "2":
+            sorgu_IPsi = input("Lütfen Sorgulamak istediğiniz IP adresnizi giriniz: ")
+            print("UYARI: Bu IP adresi 14 farkli siber saldiri veritabaninda 'Zararli' olarak işaretlenmiş! Erişim derhal kisitlaniyor")
+        else:
+            print("Yanliş seçeneği seçtiniz. Oturumuz kapatiliyor!!!")
 else:
-    print("[!] HATALI ŞİFRE! Erişim reddedildi.")
-
-    #Evet basit ama benim ilk sorgu tarzı kodum bu kodu yazarken ve ilk çıktıyı aldığımda çok eğlendim umarım sizde eğlenirsiniz :D iyi günler!
+    print(f"Sistem mesaji: bilinmeyen Ip({girilen_IP}) tespit edildi!!!")
+    anonim_istek = input("""Panelde ne yapmak istiyorsaniz seçiniz: \nSistem durumunu görmek için 1'e basiniz\nAdmin/Root Yetki başvurusu için 2'ye basiniz
+""")
+    if anonim_istek == "1":
+        print("""Sistem durumu: Çevrimiçi\nAktif kullanici sayisi 3\n1 Adet: ROOT \n2 Adet: ADMİN  
+""")
+    elif anonim_istek == "2":
+        Yetki_Talebi = input("Neden yetki istiyorsunuz ve neden biz verelim ?")
+        print("Talebiniz alindi ve Yetkililere teslim edildi. Oturumunuz sonlaniyor!!!")
+    else:
+        print("Hatali tuşlama yaptiniz. Bağlantiniz Kesiliyor! ")
